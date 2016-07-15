9273629aafd6b833cac42cebf92b87bab993aead
Maurits van Altvorst <mvanaltvorst@icloud.com>
2016-07-15 22:34:35 +0200

    Fixed some bugs and added some. There's something wrong with the pointer system and loops.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::BufReader;
 use std::env::args;
 use std::collections::BTreeMap;
+use std::io::stdout;
+use std::io::Write;
+use std::io::Read;
 
 struct Tape {
     pointer: usize,
@@ -30,6 +33,7 @@ impl Tape {
 
     fn advance(&mut self) {
         self.pointer += 1; // We assume that the brainfuck code is working
+        self.tape.push(0);
     }
 
     fn devance(&mut self) {
@@ -40,48 +44,64 @@ impl Tape {
         self.tape[self.pointer] as u8 as char
     }
 
-    fn run(&mut self, str_code: &str) {
+    fn run(&mut self, str_code: String) {
         let mut code: Vec<char> = Vec::new();
         let mut jump_map: BTreeMap<usize, usize> = BTreeMap::new();
         let mut jump_stack: Vec<usize> = Vec::new();
-        let mut pointer: usize = 0;
+        let mut pc: usize = 0;
         // Preprocessing, creating the jump map and a vector of the code
         for ch in str_code.chars() {
             match ch {
                 '>' | '<' | '+' | '-' | '.' | ',' => (),
-                '[' => jump_stack.put(pointer),
+                '[' => jump_stack.push(pc),
                 ']' => {
-                    let open = jump_stack.pop();
-                    jump_map.insert(open, pointer);
-                    jump_map.insert(pointer, open);
-                }
+                    let open = jump_stack.pop().expect("Brainfuck code is invalid ([ and ] are not equal)");
+                    jump_map.insert(open, pc);
+                    jump_map.insert(pc, open);
+                },
+                _ => ()
             }
-            code.put(ch);
-            pointer += 1;
+            code.push(ch);
+            pc += 1;
         }
-
+        println!("Created jump_map: {:#?}", jump_map);
         // The actual execution
+        let mut pc = 0;
         for ch in code {
+            println!("Matching {}", ch);
             match ch {
                 '>' => self.advance(),
                 '<' => self.devance(),
                 '+' => self.inc(),
                 '-' => self.dec(),
                 '.' => {
-                    println!("{}", ch);
-                    io::stdout().flush();
+                    println!("{}", self.get());
+                    // print!("{}", self.getchar());
+                    stdout().flush();
                 },
+                '[' => {
+                    if self.get() == 0 {
+                        self.pointer = jump_map.get(&self.pointer).unwrap().clone();
+                    }
+                },
+                ']' => {
+                    println!("End of loop, got {}", self.get());
+                    if self.get() != 0 {
+                        self.pointer = jump_map.get(&self.pointer).unwrap().clone();
+                    }
+                }
                 _ => ()
-
             }
+            pc += 1;
+            println!("{:#?}", self.tape);
         }
     }
 }
 
 fn main() {
     let path = args().nth(1).expect("Didn't get argument.");
-    println!("{}", path);
-    let file = BufReader::new(File::open(path).expect("File doesn't exist."));
-
-
+    let mut file = File::open(path).expect("File doesn't exist.");
+    let mut code: String = String::new();
+    file.read_to_string(&mut code);
+    Tape::new().run(code);
 }
