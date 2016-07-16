cf92052409aaf0d921534b896efce24ad4791b4a
Maurits van Altvorst <mvanaltvorst@icloud.com>
2016-07-16 13:07:20 +0200

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ struct Tape {
     tape: Vec<u8>
 }
 
-
-//TODO: Split Tape up
 impl Tape {
     fn new() -> Tape {
         Tape {
@@ -30,16 +28,16 @@ impl Tape {
     }
 
     fn dec(&mut self) {
-        self.tape[self.pointer] -= 1; // We assume that the brainfuck code is working
+        self.tape[self.pointer] -= 1;
     }
 
     fn advance(&mut self) {
-        self.pointer += 1; // We assume that the brainfuck code is working
+        self.pointer += 1;
         self.tape.push(0);
     }
 
     fn devance(&mut self) {
-        self.pointer -= 1; // We assume that the brainfuck code is working
+        self.pointer -= 1;
     }
 
     fn getchar(&self) -> char {
@@ -49,69 +47,56 @@ impl Tape {
     //TODO: This shouldn't be in here...
     fn run(&mut self, str_code: String) {
         let mut code: Vec<char> = Vec::new();
-        let mut jump_map: BTreeMap<usize, usize> = BTreeMap::new();
-        let mut jump_stack: Vec<usize> = Vec::new();
-        let mut pc: usize = 0; // Program counter, stores current instruction index
-        // Preprocessing, creating the jump map and a vector of the code
-        for ch in str_code.chars() {
-            match ch {
-                '>' | '<' | '+' | '-' | '.' | ',' => (),
-                '[' => jump_stack.push(pc),
-                ']' => {
-                    let open = jump_stack.pop().expect("Brainfuck code is invalid ([ and ] are not equal)");
-                    jump_map.insert(open, pc);
-                    jump_map.insert(pc, open);
-                },
-                _ => ()
+        let jump_map: BTreeMap<usize, usize> = {
+            let mut jump_map: BTreeMap<usize, usize> = BTreeMap::new();
+            let mut jump_stack: Vec<usize> = Vec::new();
+            let mut pc: usize = 0; // Program counter, stores current instruction index
+            // Preprocessing, creating the jump map and a vector of the code
+            for ch in str_code.chars() {
+                match ch {
+                    '>' | '<' | '+' | '-' | '.' | ',' => (),
+                    '[' => jump_stack.push(pc),
+                    ']' => {
+                        let open = jump_stack.pop().expect("Brainfuck code is invalid ([ and ] are not equal)");
+                        jump_map.insert(open, pc);
+                        jump_map.insert(pc, open);
+                    },
+                    _ => ()
+                }
+                code.push(ch);
+                pc += 1;
             }
-            code.push(ch);
-            pc += 1;
-        }
-        println!("Created jump_map: {:#?}", jump_map);
+            jump_map
+        };
+
         // The actual execution
-        pc = 0;
-        let mut ch: char;
-        loop {
-            ch = code[pc];
-            // println!("Matching {}", ch);
-            match ch {
+        let mut pc: usize = 0;
+        while pc < code.len() {
+            match code[pc] {
                 '>' => self.advance(),
                 '<' => self.devance(),
                 '+' => self.inc(),
                 '-' => self.dec(),
                 '.' => {
-                    print!("{}", self.getchar()); //TODO: print character
-                    // print!("{}", self.getchar());
+                    print!("{}", self.getchar());
                     stdout().flush();
                 },
                 '[' => {
-                    // println!("Began loop, {}", self.get());
                     if self.get() == 0 {
                         pc = jump_map.get(&pc).unwrap().clone();
                     }
                 },
-                ']' => {
-                    // println!("End of loop, got {}", self.get());
-                    if self.get() != 0 {
-                        // println!("PC before: {}", pc);
-                        pc = jump_map.get(&pc).unwrap().clone();
-                        // println!("PC after: {}", pc);
-                    }
-                }
+                ']' => { if self.get() != 0 { pc = jump_map.get(&pc).unwrap().clone(); } },
                 _ => ()
             }
             pc += 1;
-            // println!("{:#?}", self.tape);
-            if pc >= code.len() {
-                break;
-            }
         }
     }
 }
 
 fn main() {
     let path = args().nth(1).expect("Didn't get argument.");
-    let mut file = File::open(path).expect("File doesn't exist.");
+    let mut file = File::open(path).expect("Couldn't open file.");
     let mut code: String = String::new();
     file.read_to_string(&mut code);
     Tape::new().run(code);
